0a549e8502e17dfa331da5803fa77e717dc95a21
lccezinha <lccezinha@gmail.com>
2012-10-31 20:39:03 -0200

    compiling assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ PrimeiraApp::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( jquery.gritter.js niceEdit.js jquery.ui.widget.js jquery.pjax.js jquery.iframe-transport.js jquery.fileupload.js jquery.fileupload-ui.js )
+  config.assets.precompile += %w( jquery.gritter.js nice_edit/nicEdit.js niceEdit.js jquery.ui.widget.js jquery.pjax.js jquery.iframe-transport.js jquery.fileupload.js jquery.fileupload-ui.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
