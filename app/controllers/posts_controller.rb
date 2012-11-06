6abc3b7e43c57842d7d7719c87947914fb6f22b0
lccezinha <lccezinha@gmail.com>
2012-11-06 09:30:42 -0200

    alter other bug

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
-    @comments = @post.comments.actived?.paginate(:page => params[:page], :per_page => 5).order(:id => :desc)
+    @comments = @post.comments.actived?.paginate(:page => params[:page], :per_page => 5).order('id DESC')
     @total_comments = @post.comments.actived?.size
     @assets = @post.archives
     @comment = @post.comments.build
